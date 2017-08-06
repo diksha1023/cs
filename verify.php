54f397dd7e0da731e285eafd19f78d9f44799ff7
kira0204 <rshtmudgal@gmail.com>
2017-08-06 22:26:20 +0530

    Email verification tested and done

--- verify.php
+++ verify.php
@@ -10,19 +10,20 @@
 		$hash = trim($_GET['hash']);
 		$hash = strip_tags($hash);
 		$hash = htmlspecialchars($hash);		
-
-		$query = "SELECT uniqueid, hash, verify FROM users WHERE uniqueid='".$id."' AND hash='".$hash."' AND verify=0";
+		$query = "SELECT uniqueid, hash, verify FROM `users-test` WHERE uniqueid='".$id."' AND hash='".$hash."' AND verify='0'";
 		$result = mysqli_query($con,$query);
 		$rowcount=mysqli_num_rows($result);
 		echo $rowcount;
 		if($rowcount){
 				// We have a match, activate the account
-				mysql_query("UPDATE `users-test` SET verify='1' WHERE uniqueid='".$id."' AND hash='".$hash."' AND verify='0'") or die(mysql_error());
+				mysqli_query($con,"UPDATE `users-test` SET verify='1' WHERE uniqueid='".$id."' AND hash='".$hash."' AND verify='0'") or die(mysql_error());
 				echo '<div class="statusmsg">Your account has been activated, you can now login</div>';
 		}		
 	}
 	else {
 		echo "Error";
 	}
+	
+	mysqli_close($con);
 
 ?>
