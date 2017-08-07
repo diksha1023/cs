442ffe092c18730f5e5d60ce15cdf84cf3a91675
kira0204 <rshtmudgal@gmail.com>
2017-08-07 21:07:13 +0530

    Fixing login front-end

--- login2.php
+++ login2.php
@@ -1,27 +1,24 @@
 <?php
 require('db.php');
-if(isset($_POST['submit'])){
-	$email = trim($_POST['emailid']);
-	$email = strip_tags($email);
-	$email = htmlspecialchars($email);
 
-	$password = trim($_POST['pass']);
-	$password = strip_tags($password);
-	$password = htmlspecialchars($password);
+$email = trim($_POST['username']);
+$email = strip_tags($email);
+$email = htmlspecialchars($email);
 
-	if(empty($email)){
-		$error = true;
-		$error_msg =  "Please Enter the email address";
-	}
-	elseif (!filter_var($email,FILTER_VALIDATE_EMAIL)){
-		$error = true;
-		$error_msg = "Please enter a valid email";
-	}
-	else {
-		$query = "SELECT "
-	}	
+$password = trim($_POST['pass']);
+$password = strip_tags($password);
+$password = htmlspecialchars($password);
+
+if(empty($email)){
+	$error = true;
+	$error_msg =  "Please Enter the email address";
 }
-
-
+elseif (!filter_var($email,FILTER_VALIDATE_EMAIL)){
+	$error = true;
+	$error_msg = "Please enter a valid email";
+}
+else {
+	$query = "SELECT "
+}	
 
 ?>
